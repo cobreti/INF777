aebef8d33ba26f92d1b5a29249950adbdf4c332f
Danny Thibaudeau <cobreti_@hotmail.com>
2015-03-10 21:42:40 -0400

    hooked up menu to pages

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,5 +1,28 @@
 (function() {
 
-    $('.header-location').load('header.html');
+    var dept = window.location.pathname.split('/').length - 2;
+    var subPath = '';
+
+    while (dept > 0) {
+        subPath = subPath + '../';
+        dept --;
+    }
+
+    $('.header-location').load(subPath + 'header.html', function() {
+        console.log('done loading');
+
+        var menuLinks = $('.header-location a');
+        menuLinks.each( function(idx, element) {
+
+            console.log(element);
+            var elm = $(element);
+            var href = window.location.origin + '/' + elm.attr('page');
+            elm.attr('href', href);
+
+            if (href == window.location) {
+                console.log('current menu item should be ' + element);
+            }
+        });
+    });
 
 }());
