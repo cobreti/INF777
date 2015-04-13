0cae2441a569c5ab94c2dfaa682b2194d7ff0663
Danny Thibaudeau <cobreti_@hotmail.com>
2015-04-12 22:46:43 -0400

    moved source link to google map to javascript code

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -2,12 +2,15 @@
 
     $(document).ready( function() {
 
+        var mapSrc = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCFxrfOI16P5XfsCnpZUAUb16ydFlvFq6U&q=4101+Molson+Montreal+H1Y3L1&attribution_source=Google+Maps+Embed+API";
+
         function updateMapFrame() {
             var mapTile = $(".map");
             var mapFrame = mapTile.find('#google-map');
 
             mapFrame.attr('width', mapTile.width());
             mapFrame.attr('height', mapTile.height());
+            mapFrame.attr('src', mapSrc);
         }
 
         $(window).on('orientationchange', function() {
